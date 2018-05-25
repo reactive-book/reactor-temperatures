2a8b65e0335f642a25b62a60367cadac1f03dd11
Moises Macero <mechero@users.noreply.github.com>
2018-05-25 07:30:31 +0200

    Removing Lombok's val

--- src/main/java/io/tpd/reactivebook/reactor/ReactorApp.java
+++ src/main/java/io/tpd/reactivebook/reactor/ReactorApp.java
@@ -3,15 +3,14 @@ package io.tpd.reactivebook.reactor;
 import io.tpd.reactivebook.reactor.temperature.TemperatureLogger;
 import io.tpd.reactivebook.reactor.temperature.TemperatureProcessor;
 import io.tpd.reactivebook.reactor.temperature.TemperatureSensor;
-import lombok.val;
 
 public class ReactorApp {
 
   public static void main(String[] args) {
-    val temperatureSensor = new TemperatureSensor();
-    val temperatureSubscriberAll = new TemperatureLogger("ALL");
-    val temperatureSubscriberAvg = new TemperatureLogger("AVG");
-    val temperatureProcessor = new TemperatureProcessor(temperatureSensor.temperatureFlux());
+    final TemperatureSensor temperatureSensor = new TemperatureSensor();
+    final TemperatureLogger temperatureSubscriberAll = new TemperatureLogger("ALL");
+    final TemperatureLogger temperatureSubscriberAvg = new TemperatureLogger("AVG");
+    final TemperatureProcessor temperatureProcessor = new TemperatureProcessor(temperatureSensor.temperatureFlux());
     temperatureProcessor.process(temperatureSubscriberAll, temperatureSubscriberAvg);
   }
 
